70911c192b4bca5e94674bb2fb88be6c928d7cac
Rebne <renehgren@gmail.com>
2024-09-15 13:45:40 +0300

    Added air, added secret endpoint for adding and deleting movies also included this in the README

--- Makefile
+++ Makefile
@@ -4,5 +4,10 @@ build:
 	go build -o ${BINARY_NAME} main.go
 
 run:
-	go build -o ${BINARY_NAME} main.go
-	./${BINARY_NAME}
+	go run main.go
+
+# templ:
+# 	templ generate -watch -proxy=http://localhost:3000
+
+tailwind:
+	tailwindcss -i view/css/input.css -o public/styles.css --watch
--- main.go
+++ main.go
@@ -1,33 +1,42 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 )
 
 const API_KEY = "3cbd1470"
+const SECRET_TOKEN = "secret-token"
+const FILENAME = "id_data.txt"
 
 func main() {
-	fmt.Println(API_KEY)
-
-	component := hello("World")
-	component.Render(context.Background(), os.Stdout)
-
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		component := hello("World")
-		component.Render(r.Context(), w)
+		app := movieGenerator()
+		app.Render(r.Context(), w)
 	})
 
-	r.HandleFunc("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/secret/{token}/{action}/{id}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		name := vars["name"]
-		fmt.Fprintf(w, "Hello, %s!\n", name)
+		token := vars["token"]
+		if token != "your-secret-token" {
+			http.NotFound(w, r)
+			return
+		}
+		id := vars["id"]
+		action := vars["action"]
+		if action == "delete" {
+			deleteFromFile(id, FILENAME)
+			fmt.Fprintf(w, "Deleted %s\n", id)
+		} else if action == "add" {
+			appendToFile(id, FILENAME)
+			fmt.Fprintf(w, "Added %s\n", id)
+		} else {
+			http.NotFound(w, r)
+		}
 	})
 
 	fmt.Println("Server starting on :8080")
