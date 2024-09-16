3fa45398e73b8d26187ba0b2b2cdefd079889cda
Rebne <renehgren@gmail.com>
2024-09-16 20:20:07 +0300

    Added test picture and test data, also did some other stuff

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -17,8 +18,11 @@ const PORT = 8080
 var ids []string
 
 type MovieData struct {
-	Poster string `json:"Poster"`
-	Title  string `json:"Title"`
+	Poster  string `json:"Poster"`
+	Title   string `json:"Title"`
+	Year    string `json:"Year"`
+	Plot    string `json:"Plot"`
+	Runtime string `json:"Runtime"`
 }
 
 func fetchMovieData(id string) (MovieData, error) {
@@ -58,19 +62,24 @@ func main() {
 	})
 
 	r.Post("/clicked", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(3 * time.Second)
+		testMovieData := MovieData{
+			Poster: "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+			Title:  "Lorde of the Rings The Fellowship of the Ring",
+		}
 		id := r.FormValue("id")
 		if id == "" {
 			http.Error(w, "No ID provided", http.StatusBadRequest)
 			return
 		}
-		fmt.Printf("Received ID: %s\n", id)
-		data, err := fetchMovieData(id)
-		if err != nil {
-			http.Error(w, "Failed to fetch movie data", http.StatusInternalServerError)
-			return
-		}
+		// fmt.Printf("Received ID: %s\n", id)
+		// data, err := fetchMovieData(id)
+		// if err != nil {
+		// 	http.Error(w, "Failed to fetch movie data", http.StatusInternalServerError)
+		// 	return
+		// }
 		w.Header().Set("Content-Type", "text/html")
-		fmt.Fprintf(w, "<img id='image' src='%s' alt='Movie %s'>", data.Poster, data.Title)
+		fmt.Fprintf(w, "<img id='image' src='%s' alt='Movie %s'>", testMovieData.Poster, testMovieData.Title)
 	})
 
 	r.Get("/secret/{token}/{action}/{id}", func(w http.ResponseWriter, r *http.Request) {
